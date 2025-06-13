563f157b0cf30927e39fe2805661abc33a706669
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2025-06-13 22:30:38 +0530

    Add files via upload

--- Header.jsx
+++ Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import './Header.css'
 
 export default function Header() {
   return (
     <div>
-        <h1>This is Header Section</h1>
-        <h2>Each and every html tag must have their parent tag otherwise it'll show an error. Or the other way to use two or more tags together without any parent tag, to use those components enclosed with div tag.</h2>
+        <h1 className='header'>Header World!!</h1>
     </div>
-    
   )
 }
